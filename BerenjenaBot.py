f1fb92996bfaa027865c13d2ce3f026bd73e1965
Jorge Perianez Pascual <jorge_pery@hotmail.com>
2017-02-02 23:58:20 +0100

    Actualizado a la nueva API. Añadido comando moai

--- BerenjenaBot.py
+++ BerenjenaBot.py
@@ -1,10 +1,8 @@
-from telegram import Updater
-import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import random
 
 TOKEN = ""
-bot = telegram.Bot(TOKEN)
-updater = Updater(TOKEN)
+updater = Updater(token=TOKEN)
 updater.start_polling()
 dispatcher = updater.dispatcher
 
@@ -15,9 +13,14 @@ contador = 1
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="\xF0\x9F\x98\x8F\xF0\x9F\x8D\x86")
 
-dispatcher.addTelegramCommandHandler('start', start)
-dispatcher.addTelegramCommandHandler('berenjen', start)
-dispatcher.addTelegramCommandHandler('berenjeno', start)
+def moai(bot, update):
+	bot.sendMessage(chat_id=update.message.chat_id, text="\xF0\x9F\x97\xBF")
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('berenjen', start))
+dispatcher.add_handler(CommandHandler('berenjeno', start))
+dispatcher.add_handler(CommandHandler('moai', moai))
+dispatcher.add_handler(CommandHandler('moyai', moai))
 
 def echo(bot, update):
     global nmensaje
@@ -34,4 +37,4 @@ def echo(bot, update):
         print (contador, user_id, nombre)
         contador += 1
 
-dispatcher.addTelegramMessageHandler(echo)
+dispatcher.add_handler(MessageHandler(Filters.text, echo))
